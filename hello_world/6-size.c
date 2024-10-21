b6828083f57992cb60e1e34026bf6dd08e1a6bd9
ilmi-p <iveliu96@gmail.com>
2024-10-21 13:00:47 +0000

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.>
+/**
+ * main - to print the size of data types
+ *
+ * Return: always (0)
+ */
+int main (void)
+{
+	printf ("Size of a char: 1 byte");
+	printf ("Size of an int: 4 byte");
+	printf ("Size of an int: 4 byte");
+	printf ("Size of a long long int: 8 byte");
+	printf ("Size of a float: 4 byte");
+	return 0;
+}
