e7b6f0ea313aeea5d277b852502253b89cc3405f
ilmi-p <iveliu96@gmail.com>
2024-10-21 12:17:35 +0000

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * * main - Entry point of the program
- * *
- * * Description: Prints "with proper grammar, but the outcome is a piece of art,"
- * * * followed by a new line.
+ * * * main - Entry point of the program
+ *  * *
+ *  * * Description: Prints "with proper grammar, but the outcome is a piece of art,"
+ *  * * * followed by a new line.
  * *
  * * Return: Always 0 (Success)
  * */
