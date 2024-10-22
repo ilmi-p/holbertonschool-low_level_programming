027119620a1751d9d46c0500730d7c3576225afa
ilmi-p <iveliu96@gmail.com>
2024-10-22 09:12:23 +0000

    alphabet2

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+* main - Entry point of the program that prints the alphabet
+*
+* Description: This function prints the alphabet in lowercase,
+*followed by the alphabet in uppercase, and then a new line.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 char letter;
