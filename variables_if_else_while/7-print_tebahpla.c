e954c5263e90cb6c06586f923204651c66bd28db
ilmi-p <iveliu96@gmail.com>
2024-10-23 09:19:27 +0000

    alphabet z

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 int main(void)
-/**
-* main - Entry point of the program
-*
-* Description: This program prints the alphabet in reverse order
-* from 'z' to 'a', followed by a new line. Each letter is printed
-* using the putchar function.
-*
-* Return: Always 0 (Success)
-*/
+/* more headers goes there */
+/* betty style doc for function main goes there */
 {
 int Alphabet;
 for (Alphabet = 'z'; Alphabet >= 'a'; Alphabet--)
