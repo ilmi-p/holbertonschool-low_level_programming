1eca168596bc8cef67b05352d9620e9759698993
ilmi-p <iveliu96@gmail.com>
2024-10-23 13:06:03 +0000

    base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+** main - Entry point of the program
+*
+* Description: This program prints all hexadecimal numbers in lowercase
+* from 0 to 9 and a to f, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int base;
