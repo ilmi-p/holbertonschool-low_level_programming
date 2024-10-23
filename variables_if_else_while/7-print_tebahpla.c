4fec59d9a914c0462ca7589dd698aa733c2cc99b
ilmi-p <iveliu96@gmail.com>
2024-10-23 09:31:53 +0000

    alphabet z

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main(void)
-/* Write a program that prints the lowercase alphabet in reverse, followed by a new line. */
 {
 int Alphabet;
 for (Alphabet = 'z'; Alphabet >= 'a'; Alphabet--)
