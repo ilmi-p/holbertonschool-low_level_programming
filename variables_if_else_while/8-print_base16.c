a641054683494e8a9b76f99743b85e9988f03f06
ilmi-p <iveliu96@gmail.com>
2024-10-23 12:59:54 +0000

    base 16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -3,9 +3,10 @@ int main(void)
 {
 int base16;
 int base;
-for (base = 0; base <= 10; base++)
-putchar(base)
+for (base = 0; base < 10; base++)
+putchar(base + '0');
 for (base = 'a'; base <= 'f' ; base++)
-putchar(base16)
+putchar(base);
+putchar('\n');
 return (0);
 }
