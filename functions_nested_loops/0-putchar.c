9428cae297036465ca748c44bc229ac0cea93fb7
ilmi-p <iveliu96@gmail.com>
2024-10-24 19:59:58 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,13 @@
 #include <unistd.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar", followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-write(1,"_putchar\n",10);
+write(1, "_putchar\n", 9);
 return (0);
 }
