c70dcfd14180338c0a9992d0e6884d3c2cc26d89
ilmi-p <iveliu96@gmail.com>
2024-10-24 11:25:20 +0000

    last task of 1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,9 +4,9 @@ int main(void)
 int num;
 for (num = 0; num <= 9; num++)
 { putchar(num + '0');
-if (num !=9)
-{putchar( ',');
-putchar( ' ');
+if (num != 9)
+{putchar(',');
+putchar(' ');
 }
 }
 putchar('\n');
