8f24cbdc8d21042f82c7a42148b1e9b361c4f51c
ilmi-p <iveliu96@gmail.com>
2024-10-24 11:27:42 +0000

    last task of 1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+* main - Prints all possible combinations of single-digit numbers.
+*
+* Description: This program prints the digits from 0 to 9,
+* separated by a comma and a space, and ends with a newline.
+*
+* Return: Always 0 (Success).
+*/
 int main(void)
 {
 int num;
