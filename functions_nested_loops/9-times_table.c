fa96306c1b203c427356ef68063860e7775c8627
ilmi-p <iveliu96@gmail.com>
2024-10-26 12:58:13 +0000

    9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -4,13 +4,27 @@
  */
 void times_table(void)
 {
-int i;
-int result;
-for (i = 0; i <= 9; i++)
+int i, j, result;
+for (i = 0; i < 10; i++)
 {
-result = 9 * i;
+for (j = 0; j < 10; j++)
+{
+result = 9 * j;
+if (j == 0)
+{
 _putchar('0' + result);
+}
+else
+{
 _putchar(',');
+_putchar(' ');
+if (result < 10)
+{
+_putchar(' ');
+}
+_putchar('0' + result);
+}
+}
 _putchar('\n');
 }
 }
