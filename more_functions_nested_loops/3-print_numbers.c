98724c40f3c5a99ad0b7640c00f03ac6772e7a41
ilmi-p <iveliu96@gmail.com>
2024-10-28 13:52:22 +0000

    3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9, followed by a newline.
+ */
+void print_numbers(void)
+{
+int neuf;
+for (neuf = 0; neuf <= 9; neuf++)
+_putchar(neuf + '0');
+_putchar('\n');
+}
