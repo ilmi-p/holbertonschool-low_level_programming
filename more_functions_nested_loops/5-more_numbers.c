2046654ff0178a99537cc0bea987c46c8f07353f
ilmi-p <iveliu96@gmail.com>
2024-10-28 20:52:13 +0000

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - Prints the numbers from 0 to 14 ten times,
+ * followed by a new line each time.
+ */
+void more_numbers(void)
+{
+int i, a;
+for (i = 0; i < 10; i++)
+{
+for (a = 0; a <= 14; a++)
+{
+_putchar(a + '0');
+}
+_putchar('\n');
+}
+}
