705474cb9565b581e28f8ffff2797aad6070d143
ilmi-p <iveliu96@gmail.com>
2024-10-30 09:12:07 +0000

    1

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h> 
+/**
+ * reset_to_98 - Updates the value of an integer to 98.
+ * @n: Pointer to the integer to be updated.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+printf("adresse pointeur : %p\n", (void *)n);
+}
