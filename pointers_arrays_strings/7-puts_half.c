0810dc02dfb483a5b5a53ac4dacbb76da37d60f3
ilmi-p <iveliu96@gmail.com>
2024-11-01 14:13:21 +0000

    7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -5,14 +5,14 @@
  */
 void puts_half(char *str)
 {
-int length = 0, start;
+int length = 0, start, i;
 while (str[length] != '\0')
 length++;
 if (length % 2 == 0)
 start = length / 2;
 else
 start = (length - 1) / 2 + 1;
-for (int i = start; i < length; i++)
+for (i = start; i < length; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
