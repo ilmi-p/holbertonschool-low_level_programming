43154cf703f65083b4796d214cbd9c01d6905395
ilmi-p <iveliu96@gmail.com>
2024-11-01 08:34:48 +0000

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@
 void rev_string(char *s)
 {
 int i =0 ,a;
-while(s[i] != '\0')
+while (s[i] != '\0')
 {
 i++;
 }
 a = i-1;
 for (i=a;i >= 0;i++)
 {
-_putchar(s[i]);
+_putchar (s[i]);
 }
-_putchar('\n');
+_putchar ('\n');
 }
