93fb6372d04f9b3328fc531c042db80a23749a6b
ilmi-p <iveliu96@gmail.com>
2024-11-03 09:55:23 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,22 +11,23 @@ int i = 0;
 int sign = 1;
 int result = 0;
 int found_num = 0;
+
 while (s[i] != '\0')
 {
-if (s[i] == '-')
+if (s[i] == '-') /
 sign *= -1;
-else if (s[i] == '+')
-i++;
-if (s[i] >= '0' && s[i] <= '9')
+else if (s[i] >= '0' && s[i] <= '9')
 {
 found_num = 1;
 if (result > (2147483647 - (s[i] - '0')) / 10)
 return (sign == 1 ? 2147483647 : -2147483648);
+
 result = result * 10 + (s[i] - '0');
 }
 else if (found_num)
 break;
 i++;
 }
-return (found_num ? result * sign : 0);
+return (found_num ?0 result * sign : 0);
+}
 }
