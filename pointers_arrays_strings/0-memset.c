6d7d3fd625a6ccf2fff50832cf9e973bf498fa6e
ilmi-p <iveliu96@gmail.com>
2024-11-04 09:54:04 +0000

    0. memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,4 +1,13 @@
 #include "main.h"
+/**
+ * _memset - Remplit les n premiers octets de la zone mémoire pointée par
+ *            avec l'octet constant b.
+ * @s: Pointeur vers la zone mémoire à rempli
+ * @b: Octet constant à utiliser pour remplir la mémoir
+ * @n: Nombre d'octets à remplir
+ *
+ * Return: Pointeur vers la zone mémoire remplie (s).
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
@@ -6,6 +15,5 @@ for (i = 0 ; i < n; i++)
 {
 s[i] = b;
 }
-_putchar('\n');
 return (s);
 }
