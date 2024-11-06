f0cec23f2e81465ad19b165eb40cc4333c908f83
ilmi-p <iveliu96@gmail.com>
2024-11-06 14:19:53 +0000

    5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,22 +8,21 @@
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-{
-return -1;
+return (-1);
 return _sqrt_helper(n, 0);
 }
 /**
 * _sqrt_helper - Aide pour calculer la racine carrée en testant chaque valeur.
 * @n: Le nombre pour lequel on cherche la racine carrée.
-* @x: Le compteur qui teste les valeurs de 0 à n.
+* @x: Le compteur qui teste les valeurs de 0 |  n.
 *
 * Return: La racine carrée naturelle de n, ou -1 si elle n'existe pas.
 */
 int _sqrt_helper(int n, int x)
 {
 if (x * x == n)
-return (x);
+return x;
 if (x * x > n)
-return -1;
+return (-1);
 return (_sqrt_helper(n, x + 1));
 }
