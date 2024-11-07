9b01060082725580fadad875252c59c55ecb5def
ilmi-p <iveliu96@gmail.com>
2024-11-07 21:43:11 +0000

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,13 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
+int panda ,chat;
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int num1 = atoi(argv[1]);
-int num2 = atoi(argv[2]);
-printf("%d\n", num1 * num2);
+panda = atoi(argv[1]);
+chat  = atoi(argv[2]);
+printf("%d\n", panda * chat);
 return (0);
 }
