9d1ec13384cbde3f4a0ac6e9decbb1a412f3b6ee
ilmi-p <iveliu96@gmail.com>
2024-11-14 12:55:47 +0000

    _calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 /*
-* _calloc - Allocates memory for an array of nmemb elements of size bytes each and
-*           initializes the memory to zero.
-* @nmemb: The number of elements in the array.
-* @size: The size of each element.
-*
-* Return: A pointer to the allocated memory, or NULL if failure occurs.
+* _calloc - Alloue de la mémoire pour un tableau et initialise à z�
+* @nmemb: Nombre d'élémen
+* @size: Taille de chaque élé
+* Return: Pointeur sur la mémoire allouée ou NULL en cas d'éch
 */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 void *ptr;
+unsigned int i;
 if (nmemb == 0 || size == 0)
-return NULL;
+return (NULL);
 ptr = malloc(nmemb * size);
 if (ptr == NULL)
-return NULL;
-for (unsigned int i = 0; i < nmemb * size; i++)
+return (NULL);
+for (i = 0; i < nmemb * size; i++)
 ((char *)ptr)[i] = 0;
-return ptr;
+return (ptr);
 }
