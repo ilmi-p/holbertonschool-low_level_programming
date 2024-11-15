3baff80544e54d2868997dfb60e6adca3c2e27b4
ilmi-p <iveliu96@gmail.com>
2024-11-15 14:56:33 +0000

    1

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - A simple structure to represent a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 typedef struct dog
 {
 char *name;
