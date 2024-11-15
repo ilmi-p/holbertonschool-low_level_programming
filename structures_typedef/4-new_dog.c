dccbcae035960b955f988f8d39a8e1b8ae58cc67
ilmi-p <iveliu96@gmail.com>
2024-11-15 14:08:26 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 /**
- * new_dog - Crée un nouveau chie
- * @name: Nom du chien
- * @age: Âge du chie
- * @owner: Nom du propriétaire du chien
- *
- * Return: Pointeur vers une nouvelle structure dog_t, ou NULL si éche
+ * new_dog - crée une nouvelle structure dog_
+ * @name: nom du chien
+ * @age: âge du chie
+ * @owner: propriétaire du chie
+ * 
+ * Return: pointeur vers la nouvelle structure dog_t,
+ *         ou NULL si l'allocation ou la copie échoue
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -16,23 +16,41 @@ char *name_copy, *owner_copy;
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
-name_copy = malloc(strlen(name) + 1);
+name_copy = malloc(_strlen(name) + 1);
 if (name_copy == NULL)
 {
 free(new_dog);
 return (NULL);
 }
-strcpy(name_copy, name);
-owner_copy = malloc(strlen(owner) + 1);
+_strcpy(name_copy, name);
+owner_copy = malloc(_strlen(owner) + 1);
 if (owner_copy == NULL)
 {
 free(name_copy);
 free(new_dog);
 return (NULL);
 }
-strcpy(owner_copy, owner);
+_strcpy(owner_copy, owner);
 new_dog->name = name_copy;
 new_dog->age = age;
 new_dog->owner = owner_copy;
 return (new_dog);
+}
+int _strlen(char *s)
+{
+int len = 0;
+while (s[len])
+len++;
+return (len);
+}
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
