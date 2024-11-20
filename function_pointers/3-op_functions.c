758dbd1e6b2504d513d166cfa3f8cc1fdac86269
ilmi-p <iveliu96@gmail.com>
2024-11-20 08:46:38 +0000

    3

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -4,7 +4,7 @@
 /**
  * op_add - Calcule la somme de deux nombres
  * @a: Le premier entier
- * @b: Le deuxième entie
+ * @b: Le deuxième enti
  *
  * Return: La somme de a et b
  */
@@ -13,11 +13,11 @@ int op_add(int a, int b)
 return (a + b);
 }
 /**
- * op_sub - Calcule la différence entre deux nombre
+ * op_sub - Calcule la différence entre deux nombr
  * @a: Le premier entier
- * @b: Le deuxième entie
+ * @b: Le deuxième enti
  *
- * Return: La différence de a et 
+ * Return: La différence de a e
  */
 int op_sub(int a, int b)
 {
@@ -26,7 +26,7 @@ return (a - b);
 /**
  * op_mul - Calcule le produit de deux nombres
  * @a: Le premier entier
- * @b: Le deuxième entie
+ * @b: Le deuxième enti
  *
  * Return: Le produit de a et b
  */
@@ -37,10 +37,10 @@ return (a * b);
 /**
  * op_div - Calcule la division de deux nombres
  * @a: Le premier entier
- * @b: Le deuxième entie
+ * @b: Le deuxième enti
  *
- * Return: Le résultat de la division de a par 
- *         Quitte le programme avec un code d'erreur si b est égal à
+ * Return: Le résultat de la division de a pa
+ *         Quitte le programme avec un code d'erreur si b est éga
  */
 int op_div(int a, int b)
 {
@@ -54,10 +54,10 @@ return (a / b);
 /**
  * op_mod - Calcule le reste de la division de deux nombres
  * @a: Le premier entier
- * @b: Le deuxième entie
+ * @b: Le deuxième enti
  *
  * Return: Le reste de la division de a par b
- *         Quitte le programme avec un code d'erreur si b est égal à
+ *         Quitte le programme avec un code d'erreur si b est égal|
  */
 int op_mod(int a, int b)
 {
