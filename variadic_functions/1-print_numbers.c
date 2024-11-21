ecf213f8d97d5e045cd987f2d043e1da62c6b464
ilmi-p <iveliu96@gmail.com>
2024-11-21 11:20:57 +0000

    1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdarg.h>
-/*
- * Betty commentary:
- * This function prints numbers followed by a new line. The numbers are
- * separated by the specified separator, if provided. If separator is NULL,
- * it is not printed. The function uses variadic arguments to accept any
- * number of integers to be printed.
- */
-#include <stdio.h>
-#include <stdarg.h>
+**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers to print
+ * 
+ * Description: This function prints numbers followed by a new line.
+ * The numbers are separated by the specified separator, if provided.
+ * If separator is NULL, it is not printed. The function uses variadic
+ * arguments to accept any number of integers to be printed.
+ *
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list args;
