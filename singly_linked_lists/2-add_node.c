74eeabcc9cd3b1a00be4002172da94e8285eaabf
ilmi-p <iveliu96@gmail.com>
2024-12-04 09:11:38 +0000

    Add node no strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "lists.h"
-
 /**
  * add_node - Adds a new node at the beginning of the list.
  * @head: Pointer to the first element in the list.
@@ -12,21 +11,25 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
+list_t *new_node;
+size_t len = 0;
+new_node = malloc(sizeof(list_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+while (str[len] != '\0')
+{
+len++;
+}
+new_node->len = len;
+new_node->next = *head;
+*head = new_node;
+return (new_node);
 }
