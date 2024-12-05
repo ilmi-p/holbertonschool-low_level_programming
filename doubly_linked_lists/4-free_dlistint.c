376e3bce1345e1c2c6483f000a115fc57dd3efaa
ilmi-p <iveliu96@gmail.com>
2024-12-05 11:03:32 +0000

    Free list+ return null

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,13 @@
 void free_dlistint(dlistint_t *head)
 {
 dlistint_t *temp;
-	if (head != NULL)
+	if (head ==NULL)
+		return;
+	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
 		free(temp);
 	}
-	head = NULL;
 
 }
