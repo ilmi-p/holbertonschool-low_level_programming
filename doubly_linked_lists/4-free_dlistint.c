82e0943cfcc79a1de7dcb2af2902cc8afee4d49b
ilmi-p <iveliu96@gmail.com>
2024-12-05 10:52:19 +0000

    Free list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_dlistint - Free a doubly linked list.
+ * @head: Pointer to the head of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+	if (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	head = NULL;
+
+}
