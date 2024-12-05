c61d0e8825ded7d6655ecef785c6b2b1b559253f
ilmi-p <iveliu96@gmail.com>
2024-12-05 13:45:40 +0000

    Sum list betty

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,21 +1,24 @@
 #include "lists.h"
 /**
- * sum_dlistint - Calcule la somme des valeurs des nœuds d'une list
- *                doublement chaîné
- * @head: Pointeur vers le premier nœud de la liste.
+ * sum_dlistint - Calcule la somme des valeurs des nœuds d'une lis
+ *                doublement chaîn
+ * @head: Pointeur vers le premier nœud de la liste
  *
- * Return: La somme des valeurs des nœuds, ou 0 si la liste est vide.
+ * Return: La somme des valeurs des nœuds, ou 0 si la liste est vide
  */
 int sum_dlistint(dlistint_t *head)
 {
 	int conteneur = 0;
+
 	if (head == NULL)
-	{return (0);}
-	
+	{
+		return (0);
+	}
+
 	while (head != NULL)
 	{
 		conteneur += head->n;
 		head = head->next;
 	}
-	return (conteneur);	
+	return (conteneur);
 }
